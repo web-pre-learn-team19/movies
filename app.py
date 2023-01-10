2a76f6a87e782413015c697ee712a195447f94b8
Kuno1734 <dlsgus59@gmail.com>
2023-01-10 23:31:56 +0900

    인증서 문제로 pymongo 연결 에러 발생 certifi 패키지 추가(설치필요) , 적용

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+import certifi
+ca = certifi.where()
+
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.g596pjc.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.g596pjc.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCaFile=ca)
 db = client.dbsparta
 
 # 랭킹
