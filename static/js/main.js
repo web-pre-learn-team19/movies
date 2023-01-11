0501cd3cd20f19dd41147f27a4472d5b5341aa4c
Kuno1734 <dlsgus59@gmail.com>
2023-01-11 15:45:30 +0900

    [main] 영화 카드박스 뼈대 생성

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-    show_list();
+    show_rank();
 });
 
-function show_list(){
+function show_rank(){
     $.ajax({
         type: "GET",
         url: "/rank",
@@ -14,9 +14,24 @@ function show_list(){
                 let img_url = rows[i]['img_url']
                 let open_date = rows[i]['open_date']
 
-                let temp_html = ``
+                let temp_html = `<div class="card mb-3" style="max-width: 480px;">
+                                        <div class="row g-0">
+                                            <div class="col-md-4">
+                                                <img src="${img_url}" class="img-fluid rounded-start" alt="...">
+                                            </div>
+                                            <div class="rank_box">
+                                                <div class="col-md-8" id="rank_card">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">${title}</h5>
+                                                        <p class="card-text">${open_date}</p>
+                                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>`
                 }
-                $('#bucket-list').append(temp_html)
+                $('#rank_card').append(temp_html)
             }
     });
 }
