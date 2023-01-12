f4fc445e28914ee3136c31290ee0ed9234316358
doyeonKim <0915dy@gmail.com>
2023-01-12 14:58:51 +0900

    코드 정렬 수정 작업중

--- app.py
+++ app.py
@@ -8,71 +8,25 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.g596pjc.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
-# client2 = MongoClient('mongodb+srv://lee:sparta@Cluster0.nw7w0pd.mongodb.net/?retryWrites=true&w=majority')
-# db2 = client2.dbsparta
-
 # 랭킹
 @app.route('/')
 def home():
    return render_template('index.html')
 
+# 랭킹 크롤링
+@app.route("/rank", methods=["GET"])
+def movie_get():
+    movie_ranking = list(db.movieRank.find({}, {'_id': False}))
+
+    return jsonify({'movieRank': movie_ranking})
+
 # 리뷰
 @app.route('/review')
 def review():
-    # list(db.movieList.find({}, {'_id': False}))
     movie_list = list(db.movieRank.find({}, {'_id': False}))
-    # print(movie_list)
     movie_name = [i['title'] for i in movie_list]
-    # movie_num = [i['num'] for i in movie_list]
 
-    
     return render_template('review.html', movie_name=movie_name)
-
-# 체크리스트
-@app.route('/list')
-def checklist():
-    return render_template('list.html')
-
-@app.route("/watchlist", methods=["POST"])
-def list_post():
-    movie_receive = request.form['movie_give']
-
-    idnum = db.movieList.find_one(sort=[("num", -1)])["num"] + 1
-
-    doc = {
-        'num': idnum,
-        'movie': movie_receive,
-        'done': 0
-    }
-
-    db.movieList.insert_one(doc)
-
-    return jsonify({'msg': '등록 완료!'})
-
-@app.route("/watchlist/done", methods=["POST"])
-def list_done():
-    num_receive = request.form['num_give']
-    db.movieList.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
-    return jsonify({'msg': '체크 완료!'})
-
-@app.route("/watchlist/cancel", methods=["POST"])
-def list_cancel():
-    num_receive = request.form['num_give']
-    db.movieList.update_one({'num': int(num_receive)}, {'$set': {'done': 0}})
-    return jsonify({'msg': '체크 해제 완료!'})
-
-@app.route("/watchlist/delete", methods=["POST"])
-def list_delete():
-    num_receive = request.form['num_give']
-    db.movieList.delete_one({'num': int(num_receive)})
-    return jsonify({'msg': '삭제 완료!'})
-
-
-@app.route("/watchlist", methods=["GET"])
-def list_get():
-    watch_list = list(db.movieList.find({}, {'_id': False}))
-    return jsonify({'watchLists': watch_list})
-
 
 @app.route("/review/posting", methods=["POST"])
 def review_post():
@@ -115,18 +69,62 @@ def review_modify():
     db.movieReview.update_one({'num': int(num_receive)}, {'$set': {'comment': comment_receive}})
     return jsonify({'msg': '수정 완료!'})
 
+@app.route("/review/postingByTitle", methods=["POST"])
+def review_postingByTitle():
+    title_receive = request.form['title_give']
+    # print(title_receive)
+
+    review_list = list(db.movieReview.find({'name':title_receive}, {'_id': False}))
+    # print(review_list)
+    return jsonify({'reviews': review_list})
+
+# 체크리스트
+@app.route('/list')
+def checklist():
+    return render_template('list.html')
+
+@app.route("/watchlist", methods=["POST"])
+def list_post():
+    movie_receive = request.form['movie_give']
+
+    try:
+        idnum = db.movieReview.find_one(sort=[("num", -1)])["num"] + 1
+    except:
+        idnum = 1
+
+    doc = {
+        'num': idnum,
+        'movie': movie_receive,
+        'done': 0
+    }
+
+    db.movieList.insert_one(doc)
+
+    return jsonify({'msg': '등록 완료!'})
+
+@app.route("/watchlist/done", methods=["POST"])
+def list_done():
+    num_receive = request.form['num_give']
+    db.movieList.update_one({'num': int(num_receive)}, {'$set': {'done': 1}})
+    return jsonify({'msg': '체크 완료!'})
+
+@app.route("/watchlist/cancel", methods=["POST"])
+def list_cancel():
+    num_receive = request.form['num_give']
+    db.movieList.update_one({'num': int(num_receive)}, {'$set': {'done': 0}})
+    return jsonify({'msg': '체크 해제 완료!'})
+
+@app.route("/watchlist/delete", methods=["POST"])
+def list_delete():
+    num_receive = request.form['num_give']
+    db.movieList.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': '삭제 완료!'})
 
 
-
-# 랭킹 크롤링
-
-@app.route("/rank", methods=["GET"])
-def movie_get():
-    movie_ranking = list(db.movieRank.find({}, {'_id': False}))
-
-    return jsonify({'movieRank': movie_ranking})
-
-
+@app.route("/watchlist", methods=["GET"])
+def list_get():
+    watch_list = list(db.movieList.find({}, {'_id': False}))
+    return jsonify({'watchLists': watch_list})
 
 
 if __name__ == '__main__':
--- static/js/main.js
+++ static/js/main.js
@@ -1,50 +1,48 @@
+$(document).ready(function () {
+    listing();
+});
 
-        $(document).ready(function () {
-            listing();
-        });
+function listing() {
+    $.ajax({
+        type: 'GET',
+        url: '/rank',
+        data: {},
+        success: function (response) {
+            let rows = response["movieRank"]
+            for(let i = 0; i < rows.length; i++){
+                let title = rows[i]["title"]
+                let img = rows[i]["img"]
+                let release = rows[i]["release"]
+                let r_rate = rows[i]["r_rate"]
 
-        function listing() {
-            $.ajax({
-                type: 'GET',
-                url: '/rank',
-                data: {},
-                success: function (response) {
-                    let rows = response["movieRank"]
-                    for(let i = 0; i < rows.length; i++){
-                        let title = rows[i]["title"]
-                        let img = rows[i]["img"]
-                        let release = rows[i]["release"]
-                        let r_rate = rows[i]["r_rate"]
+                let temp_html = `<div class="col">
+                                    <div class="rating">${r_rate}</div>
+                                    <div class="card h-60 cardBox">
+                                        <img src="${img}"
+                                             class="card-img-top cardImg" >
+                                        <div class="card-body">
+                                            <h5 class="card-title">${title}</h5>
+                                            <p class="card-text">${release}</p>
+                                        </div>
+                                        <a href="/review" onclick="push_review(title)" class="reviewBtn">리뷰 확인</a>
+                                    </div>
+                                </div>`
 
-                        let temp_html = `<div class="col">
-                                            <div class="rating">${r_rate}</div>
-                                            <div class="card h-60 cardBox">
-                                                <img src="${img}"
-                                                     class="card-img-top cardImg" >
-                                                <div class="card-body">
-                                                    <h5 class="card-title">${title}</h5>
-                                                    <p class="card-text">${release}</p>
-                                                </div>
-                                                <a href="/review" onclick="push_review('${title}')" class="reviewBtn">리뷰 확인</a>
-                                            </div>
-                                        </div>`
-
-                        $('#cards-box').append(temp_html)
-                    }
-                }
-            })
+                $('#cards-box').append(temp_html)
+            }
         }
+    })
+}
 
-        function push_review(title) {
-            $.ajax({
-                type: 'POST',
-                url: '/',
-                data: {},
-                url: '/rank/push_review',
-                data: {title_give: title},
-                success: function (response) {
-                    alert(response['msg'])
-                    window.location.reload()
-                }
-            });
+function push_review(title) {
+    console.log(title)
+    $.ajax({
+        type: 'POST',
+        url: '/rank/push_review',
+        data: {title_give: title},
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
         }
+    });
+}
